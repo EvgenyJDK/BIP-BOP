f9ca608464f43757af3858d086888f8821ad3710
Evgeny <evgenyjdk@gmail.com>
2018-03-05 18:05:38 +0200

    1st screen Design

--- BipBop/ViewController.swift
+++ BipBop/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BeginViewController: UIViewController {
 
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var centerView: UIView!
+    @IBOutlet weak var beginButton: UIButton!
+    
+    private let topColor = UIColor(red: 102/255, green: 112/255, blue: 241/255, alpha: 1)
+    private let bottomColor = UIColor(red: 184/255, green: 77/255, blue: 235/255, alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configurateUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func configurateUI() {
+        backgroundView.addGradientWithColor(topColor: topColor, bottomColor: bottomColor)
+        centerView.setRadius()
+        beginButton.titleLabel?.textAlignment = .center
     }
+    
+    @IBAction func action(_ sender: Any) {
+        print("action")
+    }
+    
+}
 
 
+extension UIView {
+    
+    func setRadius(radius: CGFloat? = nil) {
+        self.layer.cornerRadius = radius ?? self.frame.width / 2;
+        self.layer.masksToBounds = true;
+    }
+    
+    func addGradientWithColor(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = UIScreen.main.bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        self.layer.insertSublayer(gradient, at: 0)
+    }
 }
 
